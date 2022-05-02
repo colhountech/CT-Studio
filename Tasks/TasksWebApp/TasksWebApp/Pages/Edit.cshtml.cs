c5b44608e9593d29023a2683b6495ff45c340b54
Micheal Colhoun <micheal@cts-ni.com>
2022-05-02 17:10:50 +0100

    Add Edit Page

---
+++ Tasks/TasksWebApp/TasksWebApp/Pages/Edit.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TasksWebApp.Services;
+using TasksWebApp.ViewModels;
+
+namespace TasksWebApp.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IToDoItemService _service;
+        private readonly IMapper _mapper;
+
+        public EditModel(ILogger<IndexModel> logger, IToDoItemService service, IMapper mapper)
+        {
+            _logger = logger;
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [BindProperty]
+        public TodoItemViewModel? TodoItem { get; set; }
+
+
+        public IActionResult OnGet(Guid? ID)
+        {
+            if (ID == null)
+            {
+                return NotFound();
+            }
+
+            var itemData = _service.GetItemByID(ID.Value);
+
+            if (itemData == null)
+            {
+                return NotFound();
+            }
+
+            TodoItem = _mapper.Map<TodoItemViewModel>(itemData);
+
+            return Page();
+
+        }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        if (TodoItem != null)
+        {
+            var itemData = _mapper.Map<TodoItemData>(TodoItem);
+            _service.UpdateItem(TodoItem.ID, itemData);
+        }
+
+
+        return RedirectToPage("Index");
+    }
+}
+}
