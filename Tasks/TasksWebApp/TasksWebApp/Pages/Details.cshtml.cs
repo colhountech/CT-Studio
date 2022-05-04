bf4e465986e7af0dfeb13ca3835c2ce7c69aadc3
Micheal Colhoun <micheal@cts-ni.com>
2022-05-04 18:02:49 +0100

    Add Details Page

---
+++ Tasks/TasksWebApp/TasksWebApp/Pages/Details.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TasksWebApp.Services;
+using TasksWebApp.ViewModels;
+
+namespace TasksWebApp.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IToDoItemService _service;
+        private readonly IMapper _mapper;
+
+        public DetailsModel(ILogger<IndexModel> logger, IToDoItemService service, IMapper mapper)
+        {
+            _logger = logger;
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [BindProperty]
+        public TodoItemViewModel? TodoItem { get; set; }
+
+
+        public IActionResult OnGet(Guid? ID)
+        {
+            if (ID == null)
+            {
+                return NotFound();
+            }
+
+            var itemData = _service.GetItemByID(ID.Value);
+
+            if (itemData == null)
+            {
+                return NotFound();
+            }
+
+            TodoItem = _mapper.Map<TodoItemViewModel>(itemData);
+
+            return Page();
+
+        }
+    }
+}
