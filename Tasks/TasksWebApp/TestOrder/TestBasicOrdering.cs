d12246a72ed9d5622c4d490604b8d5799e3a9c98
Micheal Colhoun <micheal@cts-ni.com>
2022-05-10 12:50:06 +0100

    Basic Ordering Demo

---
+++ Tasks/TasksWebApp/TestOrder/TestBasicOrdering.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestOrder
+{
+    public class TestBasicOrdering
+    {
+        private readonly ITestOutputHelper output;
+
+        public TestBasicOrdering(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void TestOrderNumbers()
+        {
+            int[] N = { 1, 5, 34, 2, 5, 6, 8, 9, 33, 7};
+
+
+            int t = 0;
+
+            for (int i = 0; i < N.Length; i++)
+            {
+                for (int j = i+1; j < N.Length; j++)
+                {
+                    if ( N[i] > N[j])
+                    {
+                        t = N[i];       // save i
+                        N[i] = N[j];    // swap j with i
+                        N[j] = t;       // swap i with j         
+                    }
+                    int k = 0;
+                    output.WriteLine($"Loop[{i}] {N[k++]} {N[k++]} {N[k++]} {N[k++]} {N[k++]} {N[k++]} {N[k++]} {N[k++]} {N[k++]} {N[k++]} ");
+                }
+            }
+
+        }
+
+        public class Anything
+        {
+            public string Name { get; set; }
+            public int Order { get; set; }
+
+        }
+
+        [Fact]
+        public void TestOrderAnything()
+        {
+            var stuff = new List<Anything> {
+                new Anything{ Name = "meh", Order = 3 },
+                new Anything{ Name = "more meh", Order = 4 },
+                new Anything{ Name = "high priority", Order = 10 },
+                new Anything { Name = "low priority", Order = 1 },
+                new Anything{ Name = "medium", Order = 5 },
+                new Anything{ Name = "kinda important", Order = 6 },
+                };
+
+            output.WriteLine("Not ordered... Yet!");
+            foreach (var s in stuff)
+            {
+                output.WriteLine($"{s.Name} == {s.Order}");
+            }
+
+            Order(stuff);
+
+            output.WriteLine("\nOrdered Correctly!");
+            foreach (var s in stuff)
+            {
+                output.WriteLine($"{s.Name} == {s.Order}");
+            }
+        }
+
+        private void Order(List<Anything> stuff)
+        {
+            for (int i = 0; i < stuff.Count; i++)
+            {
+                for (int j = i + 1; j < stuff.Count; j++)
+                {
+                    if (stuff[i].Order > stuff[j].Order)
+                    {
+                        Swap<Anything>(stuff, i, j);
+                    }
+
+                }
+            }
+
+
+        }
+        private void Swap<T>(List<T> list, int left, int right)
+        {
+            T temp;
+            temp = list[left];
+            list[left] = list[right];
+            list[right] = temp;
+        }
+
+
+    }
+}
