42e19707db33b64f624b0d9c10c77f8a49e1440f
Micheal Colhoun <micheal@cts-ni.com>
2022-06-05 11:23:26 +0100

    Serve SwaggerUI from root

--- Tasks/TasksWebApi/Program.cs
+++ Tasks/TasksWebApi/Program.cs
@@ -22,7 +22,13 @@ namespace TasksWebApi
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                //app.UseSwaggerUI();
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
             }
 
             app.MapControllers();
