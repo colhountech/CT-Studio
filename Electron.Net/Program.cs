5c9786d6ec0136bda207a91162617b171e274cd0
Micheal Colhoun <micheal@cts-ni.com>
2022-06-12 14:30:04 +0100

    ElectronNET.API Startup using ASP.NET Core 6.0 minimal Api

--- Electron.Net/Program.cs
+++ Electron.Net/Program.cs
@@ -1,25 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
+using ElectronNET.API;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+await Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder.UseElectron(args);
+        webBuilder.UseStartup<Startup>();
+    })
+    .Build()
+    .RunAsync();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
